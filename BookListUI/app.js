e26530e0e6732b5e5e8a6d28430b7a317e5e6b0c
Roopak Chugh <chughroopak@gmail.com>
2019-03-20 11:12:41 +0530

    JS functionality 1

---
+++ BookListUI/app.js
@@ -0,0 +1,77 @@
+//Book constructor
+
+function Book(title, author, isbn){
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
+}
+
+//UI Constructor
+function UI(){
+}
+
+UI.prototype.addbookToList = function(book){
+  const list = document.getElementById('book-list');
+  const row = document.createElement('tr');
+  //Insert cols
+
+  row.innerHTML= `
+  <td>
+  ${book.title}
+  </td>
+  <td>
+  ${book.author}
+  </td>
+  <td>
+  ${book.isbn}
+  </td>
+  <td>
+  <a href="#" class="delete">X</a></td>
+  </td>`
+  console.log(row);
+  list.appendChild(row);
+}
+
+UI.prototype.clearFields = function(){
+  document.getElementById('title').value="";
+  document.getElementById('author').value="";
+  document.getElementById('isbn').value="";
+}
+
+UI.prototype.showAlert = function(message, className){
+  const div = document.createElement('div');
+  div.className= `alert ${className}`;
+
+  div.appendChild(document.createTextNode(message));
+
+  const container = document.querySelector('.container');
+  const form = document.querySelector('#book-form');
+  container.insertBefore(div, form);
+
+  setTimeout(function(){
+    document.querySelector('.alert').remove();
+  },3000);
+}
+
+
+document.getElementById('book-form').addEventListener('submit', function(e){
+  const title = document.getElementById('title').value,
+        author = document.getElementById('author').value,
+        isbn = document.getElementById('isbn').value;
+
+  const book = new Book(title, author, isbn);
+  
+  // Instantiate UI
+  const ui = new UI();
+  
+  //Validate
+  if(title === '' || author === '' || isbn === ''){
+    ui.showAlert('Please fill in all fields', 'error');
+  } else {
+    //Add book to list
+    ui.addbookToList(book);
+    ui.showAlert('Book Added!', 'success');
+    ui.clearFields();
+  }
+  e.preventDefault();
+});
